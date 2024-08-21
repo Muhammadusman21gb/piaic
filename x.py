e3ddb65d20e0752f5f609705b53abfa6c2742bc4
Muhammadusman21gb <m4usmanm21@gmail.com>
2024-08-21 16:48:02 -0700

    add new program

--- x.py
+++ x.py
@@ -1,6 +1,5 @@
- #- Write a program that calculates the percentage
-originalValue = float(input("enter original value"))
-newValue = float(input("enter new value"))
-percentage = newValue*100/originalValue
-print(percentage)
-
+# Write a program that calculates the BMI using height (in meters) and weight (in kilograms) variable
+height = int(input("enter height in meters"))
+weight = int(input("enter weight in kg"))
+BMI = weight / height**2
+print(BMI)
