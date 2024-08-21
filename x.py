6624cb453cdbb6716bf8a40af8a17fc2e2a811dd
Muhammadusman21gb <m4usmanm21@gmail.com>
2024-08-20 23:58:54 -0700

    add new program

--- x.py
+++ x.py
@@ -1,3 +1,5 @@
-radius =int(input("enter area of circle"))
-area = 3.14 *(radius**2)
-print(area)
+# Write a program that calculates the area of the cube
+
+length = input(" area of cube")
+cubeArea = 6 * (length ** 2)
+print("cubeArea")
