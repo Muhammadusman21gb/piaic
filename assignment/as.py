f649a0d46bd56681dd62f0de3f106b4b537bb492
Muhammadusman21gb <m4usmanm21@gmail.com>
2024-08-25 17:16:37 -0700

    add new program

---
+++ assignment/as.py
@@ -0,0 +1,7 @@
+  #- Convert a given number of seconds into minutes and seconds using variables.
+inputNumber = int(input("enter your number"))
+result = inputNumber * 60
+print(result)
+inputNumber = int(input("enter your number"))
+result = inputNumber / 60 
+print(result)
