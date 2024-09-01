baca82a639b09ab5e18dadf8f36e31902f0186cd
Muhammadusman21gb <m4usmanm21@gmail.com>
2024-09-01 14:35:02 -0700

    file updated

--- x.py
+++ x.py
@@ -1,7 +1,33 @@
-#Write a program that calculates the volume of a cylinder using the formula .
-height = int(input("enter height of cylinder"))
-radius = int(input("enter radius of cylinder"))
-π = 3.14
-Volume  =π * (radius**2) * height
-print(Volume)
+#  #- Convert a given number of seconds into minutes and seconds using variables.
+# #inputNumber = int(input("enter your number"))
+# #result = inputNumber * 60
+# #print(result)
+# #inputNumber = int(input("enter your number"))
+# #result = inputNumber / 60 
+# #print(result)
+ 
+# def add(num3,num4):
+#     num1 = 20
+#     num2 = 40
+#     result = num3 + num4
+#     print(result)
+#     def divide():
+#         num3 = 20
+#         num4 = 80
+#         result2 = num3 % num4
+#         print(result2)
+#     divide()
+# add(10,30 )
+# def add (num1,num2):
+#     result = num1 + num2
+#     return result
+# output = add(50,30)
+# finalOutput = add(output,70)
+# print(finalOutput)
 
+def Calculate_age(ageYear:int)->int:
+    currentYear:int = 2024
+    return currentYear - ageYear
+ageYear:int = int(input("Enter Age Year: "))
+age:int = Calculate_age(ageYear)
+print("Your age is: ", age)    
